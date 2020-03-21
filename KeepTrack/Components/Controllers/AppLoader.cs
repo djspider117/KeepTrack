5d932e75db54134f45872f25087d38dce85eba07
John Popovici <ionut.popovici@engageworks.com>
2020-03-22 00:04:28 +0200

    started implementing app loader

---
+++ KeepTrack/Components/Controllers/AppLoader.cs
@@ -0,0 +1,92 @@
+﻿using KeepTrack.Core;
+using KeepTrack.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace KeepTrack.Components.Controllers
+{
+    public interface IAppContextManager
+    {
+        IKTAppContext Context { get; set; }
+
+        Task LoadAsync();
+        Task SaveAsync();
+    }
+
+    public class AppContextManager : IAppContextManager
+    {
+        public const string APP_DB_PATH = "data.json";
+
+        public JsonSerializerSettings SerializerSettings { get; set; }
+        public IKTAppContext Context { get; set; }
+
+        public AppContextManager()
+        {
+            SerializerSettings = new JsonSerializerSettings()
+            {
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.Objects,
+                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+                DateFormatHandling = DateFormatHandling.IsoDateFormat,
+            };
+        }
+
+        public async Task LoadAsync()
+        {
+            var dbFile = await StorageFile.GetFileFromPathAsync(APP_DB_PATH);
+            var seri = await FileIO.ReadTextAsync(dbFile);
+
+            var context = await Task.Run(() =>
+               {
+                   IKTAppContext ctx = null;
+                   try
+                   {
+                       ctx = JsonConvert.DeserializeObject<KTAppContext>(seri, SerializerSettings);
+                   }
+                   catch (Exception ex)
+                   {
+                       // TODO add error handling
+                       Debug.WriteLine(ex);
+                   }
+                   return ctx;
+               });
+
+            Context = context ?? throw new KTException("APpContext is null.");
+        }
+
+        public async Task SaveAsync()
+        {
+            if (Context == null)
+                return;
+
+            var dbFile = await StorageFile.GetFileFromPathAsync(APP_DB_PATH);
+
+            var seri = await Task.Run(() =>
+            {
+                string ctx = null;
+                try
+                {
+                    ctx = JsonConvert.SerializeObject(Context, SerializerSettings);
+                }
+                catch (Exception ex)
+                {
+                    // TODO add error handling
+                    Debug.WriteLine(ex);
+                }
+                return ctx;
+            });
+
+            if (seri == null)
+                throw new KTException("Unable to serialize AppContext");
+
+            await FileIO.WriteTextAsync(dbFile, seri);
+        }
+    }
+}
