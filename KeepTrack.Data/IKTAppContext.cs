9c4a0ee5697715b33c1e7731fd06cc524cc2fc41
John Popovici <ionut.popovici@engageworks.com>
2020-03-22 01:08:04 +0200

    separated IKTAppContext interface

---
+++ KeepTrack.Data/IKTAppContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace KeepTrack.Data
+{
+    public interface IKTAppContext
+    {
+        IList<KTIcon> Icons { get; set; }
+        IList<KTActivityType> Activities { get; set; }
+        IList<KTProject> Projects { get; set; }
+        IList<KTTask> Tasks { get; set; }
+        IList<KTTimeline> Timelines { get; set; }
+        IList<KTTimeEvent> TimeEvents { get; set; }
+    }
+}
--- KeepTrack.Data/KTAppContext.cs
+++ KeepTrack.Data/KTAppContext.cs
@@ -4,16 +4,6 @@ using System.Text;
 
 namespace KeepTrack.Data
 {
-    public interface IKTAppContext
-    {
-        IList<KTIcon> Icons { get; set; }
-        IList<KTActivityType> Activities { get; set; }
-        IList<KTProject> Projects { get; set; }
-        IList<KTTask> Tasks { get; set; }
-        IList<KTTimeline> Timelines { get; set; }
-        IList<KTTimeEvent> TimeEvents { get; set; }
-    }
-
     public class KTAppContext : IKTAppContext
     {
         public IList<KTIcon> Icons { get; set; }
