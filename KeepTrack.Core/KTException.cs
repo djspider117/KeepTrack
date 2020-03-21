882045cf9e387b8f5a6fc81d41feacce28cba854
John Popovici <ionut.popovici@engageworks.com>
2020-03-22 00:04:15 +0200

    added generic app exception

---
+++ KeepTrack.Core/KTException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeepTrack.Core
+{
+
+    [Serializable]
+    public class KTException : Exception
+    {
+        public KTException() { }
+        public KTException(string message) : base(message) { }
+        public KTException(string message, Exception inner) : base(message, inner) { }
+        protected KTException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
